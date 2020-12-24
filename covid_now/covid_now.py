0effd80efee4802c3ec881c11c690e940578cafc
Yuma Fujisawa <mahuuzinn@gmail.com>
2020-12-24 20:58:16 +0900

    add show_graph

--- covid_now/covid_now.py
+++ covid_now/covid_now.py
@@ -1,16 +1,34 @@
 import csv
 import requests
+from operator import itemgetter
 
-DATA_LOCATION = '../data/owid-covid-latest.csv'
+DATA_LOCATION = '/tmp/covid_now/owid-covid-latest.csv'
 URL = 'https://github.com/owid/covid-19-data/raw/master/public/data/latest/owid-covid-latest.csv'
 
-with open(DATA_LOCATION) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    for row in reader:
-        print(row['location'], row['total_cases'])
+
+# with open(DATA_LOCATION) as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+#     for row in reader:
+#         print(row['location'], row['total_cases'])
 
 
 def csv_getter(url):
     r = requests.get(url)
     decoded_content = r.content.decode('utf-8')
     return decoded_content.splitlines()
+
+
+def show_graph(csv_data):
+    reader = csv.DictReader(csv_data, delimiter=',', quotechar='"')
+    graph_data = list()
+    for row in reader:
+        try:
+            graph_data.append([row['location'], float(row['total_cases'])])
+        except ValueError:
+            graph_data.append([row['location'], 0])
+
+    graph_data.sort(key=itemgetter(1))
+    print(graph_data)
+
+
+show_graph(csv_getter(URL))
